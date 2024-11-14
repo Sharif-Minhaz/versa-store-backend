486384785ff2d75a469b5d6bcb938dd0b9329615
Minhaz <sharifmdminhazurrahman@gmail.com>
2024-11-14 21:18:03 +0600

    fix: upload middleware

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,20 +1,8 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
-
-// Define the upload directory
-const uploadDir = path.join(__dirname, "uploads");
-
-// Ensure the upload directory exists
-if (!fs.existsSync(uploadDir)) {
-	fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 // Configure storage for multer
 const storage = multer.diskStorage({
-	destination: function (_req, _file, cb) {
-		cb(null, uploadDir);
-	},
 	filename: function (_req, file, cb) {
 		cb(null, `${file.fieldname}-${Date.now()}-${file.originalname}`);
 	},
